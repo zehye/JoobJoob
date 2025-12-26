6f49a3e162264da46b82cc07a9ee47365f8f0c57
jihye <zehye.01@gmail.com>
2025-12-26 19:00:57 +0900

    이미지, 컬러 추가

--- Joobjoob/Extensions/UIColor+Ex.swift
+++ Joobjoob/Extensions/UIColor+Ex.swift
@@ -5,3 +5,99 @@
 //  Created by zehye on 12/26/25.
 //
 
+import UIKit
+
+extension UIColor {
+    
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        self.init(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+    
+    convenience init(red: Int, green: Int, blue: Int, alpha: CGFloat) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: alpha)
+    }
+    
+    convenience init(netHex: Int) {
+        self.init(red: (netHex >> 16) & 0xff, green: (netHex >> 8) & 0xff, blue: netHex & 0xff)
+    }
+    
+    convenience init(hex: String, alpha: CGFloat) {
+        var hexStr = hex
+        hexStr = hexStr.replacingOccurrences(of: "#", with: "")
+        let scanner = Scanner(string: hexStr as String)
+        var color: UInt64 = 0
+        if scanner.scanHexInt64(&color) {
+            let r = CGFloat((color & 0xFF0000) >> 16)
+            let g = CGFloat((color & 0x00FF00) >> 8)
+            let b = CGFloat(color & 0x0000FF)
+            
+            self.init(red: Int(r), green: Int(g), blue: Int(b), alpha: alpha)
+        } else {
+            // Invalid hex string
+            self.init(red: 0, green: 0, blue: 0)
+        }
+    }
+    
+    convenience init(hex: String) {
+        var hexStr = hex
+        // let alpha:CGFloat = 1.0
+        hexStr = hexStr.replacingOccurrences(of: "#", with: "")
+        let scanner = Scanner(string: hexStr as String)
+        var color: UInt64 = 0
+        if scanner.scanHexInt64(&color) {
+            let r = CGFloat((color & 0xFF0000) >> 16)
+            let g = CGFloat((color & 0x00FF00) >> 8)
+            let b = CGFloat(color & 0x0000FF)
+            
+            self.init(red: Int(r), green: Int(g), blue: Int(b))
+        } else {
+            // Invalid hex string
+            self.init(red: 0, green: 0, blue: 0)
+        }
+    }
+    
+}
+
+extension UIColor {
+    /**
+     hex로 UIColor 만들기
+     
+     - parameter hex: Int
+     */
+    convenience init(hex: Int) {
+        self.init(red:(hex >> 16) & 0xff, green:(hex >> 8) & 0xff, blue:hex & 0xff)
+    }
+    
+    /**
+     hex로 UIColor 만들기
+     
+     - parameter hex: String
+     */
+    convenience init(hexString: String) {
+        
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        
+        switch hex.count {
+        case 3:
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6:
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8:
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
+}
